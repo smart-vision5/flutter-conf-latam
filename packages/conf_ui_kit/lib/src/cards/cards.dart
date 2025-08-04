7f43ab1e2ecc59ab7618ca418fa6df0ecda0ec2f
Smart <smart.vision0319@gmail.com>
2025-08-04 18:38:43 -0500

    feat: export SessionSpeakerCard and SessionSpeakerInfoCard in cards.dart

--- packages/conf_ui_kit/lib/src/cards/cards.dart
+++ packages/conf_ui_kit/lib/src/cards/cards.dart
@@ -1,5 +1,7 @@
 export 'info_card.dart';
 export 'section_navigation_card.dart';
 export 'session_card.dart';
+export 'session_speaker_card.dart';
+export 'session_speaker_info_card.dart';
 export 'speaker_card.dart';
 export 'speaker_languages_card.dart';
