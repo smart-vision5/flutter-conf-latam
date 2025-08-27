09b4cfcb4b57402638a60d10c5a31b7e56998464
Smart <smart.vision0319@gmail.com>
2025-08-27 18:09:53 -0500

    chore: remove unused localization and country details from SpeakerDetailsHeaderSection

--- lib/speaker_details/widgets/speaker_details_header_section.dart
+++ lib/speaker_details/widgets/speaker_details_header_section.dart
@@ -1,7 +1,6 @@
 import 'package:conf_shared_models/conf_shared_models.dart' show SpeakerSummary;
 import 'package:conf_ui_kit/conf_ui_kit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_conf_latam/l10n/l10n.dart';
 
 class SpeakerDetailsHeaderSection extends StatelessWidget {
   const SpeakerDetailsHeaderSection({required this.speakerSummary, super.key});
@@ -10,13 +9,10 @@ class SpeakerDetailsHeaderSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final l10n = context.l10n;
     final textTheme = context.textTheme;
 
     final name = speakerSummary.name;
     final title = speakerSummary.title;
-    final country = speakerSummary.country;
-    final countryFlag = speakerSummary.flagEmoji;
 
     return SliverToBoxAdapter(
       child: Padding(
@@ -26,7 +22,6 @@ class SpeakerDetailsHeaderSection extends StatelessWidget {
             SpeakerAvatar(
               speaker: speakerSummary,
               size: UiConstants.avatarSizeXLarge,
-              showFlag: false,
             ),
             const SizedBox(width: UiConstants.spacing16),
             Flexible(
@@ -40,23 +35,6 @@ class SpeakerDetailsHeaderSection extends StatelessWidget {
                     ),
                   ),
                   Text(title),
-                  MergeSemantics(
-                    child: Row(
-                      children: [
-                        Semantics(
-                          label: l10n.countryFlag(country),
-                          child: Text(
-                            countryFlag,
-                            style: const TextStyle(
-                              fontSize: UiConstants.fontSizeXLarge,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: UiConstants.spacing4),
-                        Text(country),
-                      ],
-                    ),
-                  ),
                 ],
               ),
             ),
