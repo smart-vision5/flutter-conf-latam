f07316d1a052bbb52510614a5ecf6a7a2086324b
Smart <smart.vision0319@gmail.com>
2025-08-27 17:12:01 -0500

    chore: update SessionType enum to include additional session types

--- packages/conf_shared_models/lib/src/enums/enums.dart
+++ packages/conf_shared_models/lib/src/enums/enums.dart
@@ -2,6 +2,17 @@ enum SessionLevel { basic, intermediate, advanced, expert }
 
 enum Language { spanish, english }
 
-enum SessionType { checkIn, keynote, session, breakTime, workshop, breaks }
+enum SessionType {
+  checkIn,
+  keynote,
+  session,
+  workshop,
+  breaks,
+  lunch,
+  coffee,
+  networking,
+  panel,
+  finish,
+}
 
 enum SponsorTier { platinum, gold, silver, inKind, senior, other }
